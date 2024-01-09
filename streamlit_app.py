dcfb46c9627e8258147b5b73fbd58ad48e592a9b
alphasec <95212748+alphasecio@users.noreply.github.com>
2024-01-09 09:45:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import yfinance as yf
 
-st.title("Stocks App")
+st.subheader("Stocks App")
 symbol = st.text_input("Enter a stock symbol", "AAPL")
 if st.button("Get Quote"):
     st.json(yf.Ticker(symbol).info)
